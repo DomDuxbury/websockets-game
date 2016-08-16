6cef09a24dac2f9761a88deb332593bb2f73e58e
DomDuxbury <domduxbury@hotmail.com>
2016-08-16 16:07:39 +0100

    Added handlebars templating

--- source/main.js
+++ source/main.js
@@ -3,25 +3,34 @@
 const koa = require('koa');
 const app = koa();
 const serve = require('koa-static');
+const views = require('koa-views');
 const server = require('http').createServer(app.callback());
 const io = require('socket.io')(server);
 
+
 let users = 0;
+app.use(views(__dirname + '/client/views', {
+  map: {
+    html: 'handlebars'
+  }
+}));
 
 io.on('connection', function (socket){
   
-  users++;
+  users++; 
+  console.log(`There are ${users} current users`);
   
   socket.on('disconnect', function(){
     users--;
+    console.log(`There are ${users} current users`);
   });
   
-  console.log(`There are ${users} current users`);
 });
 
-app.use(serve(__dirname + '/client', {defer: true}));
-
 app.use(function *() {
+  yield this.render('user', {
+    user: 'John'
+  });
 });
  
 server.listen(3000);
