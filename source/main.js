a7196757df9faa17043ace43bbabd9de51cdd31e
DomDuxbury <domduxbury@hotmail.com>
2016-08-16 16:51:55 +0100

    Broadcasting current user count

--- source/main.js
+++ source/main.js
@@ -4,19 +4,19 @@ const koa = require('koa');
 const app = koa();
 const serve = require('koa-static');
 const views = require('koa-views');
-app.use(serve(__dirname + '/client'))
 const server = require('http').createServer(app.callback());
 const io = require('socket.io')(server);
 
 
 let users = 0;
 
+app.use(serve(__dirname + '/client'))
 
 io.on('connection', function (socket){
   
   users++; 
   console.log(`There are ${users} current users`);
-  
+  io.emit('connect-message', users)
   socket.on('disconnect', function(){
     users--;
     console.log(`There are ${users} current users`);
