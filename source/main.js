3f6ddae6292eb3b0f598595ff611cddee7b3882b
DomDuxbury <domduxbury@hotmail.com>
2016-08-16 14:43:53 +0100

    Updated shape of main.js

--- source/main.js
+++ source/main.js
@@ -2,14 +2,14 @@ var koa = require('koa');
 var app = koa();
 
 app.use(function *(next){
-    var start = new Date;
-      yield next;
-        var ms = new Date - start;
-          console.log('%s %s - %s', this.method, this.url, ms);
+  var start = new Date;
+  yield next;
+  var ms = new Date - start;
+  console.log('%s %s - %s', this.method, this.url, ms);
 });
 
 app.use(function *(){
-    this.body = 'Hello World';
+  this.body = 'Hello World';
 });
  
 app.listen(3000);
