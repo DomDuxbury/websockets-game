9ccdb822d5df826de35d563542acb09de87b305f
DomDuxbury <domduxbury@hotmail.com>
2016-08-16 14:52:26 +0100

    Added current user count

--- source/main.js
+++ source/main.js
@@ -1,15 +1,11 @@
-var koa = require('koa');
-var app = koa();
+'use strict';
 
-app.use(function *(next){
-  var start = new Date;
-  yield next;
-  var ms = new Date - start;
-  console.log('%s %s - %s', this.method, this.url, ms);
-});
+const koa = require('koa');
+const app = koa();
 
-app.use(function *(){
-  this.body = 'Hello World';
+app.use(function *() {
+  let users = 0;
+  this.body = `There are ${users} current users`;
 });
  
 app.listen(3000);
